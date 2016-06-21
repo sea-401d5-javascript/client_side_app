eb5f728166d349e432a42237d4edbbbd410a8ecc
jdar22169 <mmow313@gmail.com>
2016-06-21 12:59:50 -0700

    api server changes

--- Maddie/api_server.js
+++ Maddie/api_server.js
@@ -5,7 +5,6 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const frenchieRouter = require('./routes/frenchieRouter.js');
 const dogWalkerRouter = require('./routes/dogWalkerRouter.js');
-const mixedRouter = require('./routes/mixedRouter.js');
 const dbPort = process.env.MONGOLAB_URI || 'mongodb://localhost/dev_db';
 
 mongoose.connect(dbPort);
@@ -13,7 +12,6 @@ mongoose.connect(dbPort);
 app.use(cors());
 app.use('/frenchie', frenchieRouter);
 app.use('/dogwalkers', dogWalkerRouter);
-app.use('/unfortunate', mixedRouter);
 app.use((err,req,res,next) => {
   res.status(404).json({message:err.message});
   next(err);
