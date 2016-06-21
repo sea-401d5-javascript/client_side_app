3602e4f5fb11dc21512d664cc3d37c2beb2dc034
Kevin Chuang <kcxinkun@gmail.com>
2016-06-21 09:32:53 -0700

    get, post, and delete routes working for cookies, still have trouble with put

--- kevinChuang/build/bundle.js
+++ kevinChuang/build/bundle.js
@@ -31554,97 +31554,99 @@
 
 
 	function TreatsController($http) {
-	  const cookieUrl = 'http://localhost:3000/cookies';
-	  const candyUrl = 'http://localhost:3000/candy';
+	  this.$http = $http;
+	  this.cookieUrl = 'http://localhost:3000/cookies';
+	  this.candyUrl = 'http://localhost:3000/candy';
 	  this.treats = [];
-
-	  // GET routes------------------------------------
-
-	  TreatsController.prototype.getCookies = function() {
-	    $http.get(cookieUrl)
-	      .then((res) => {
-	        this.treats = res.data.data;
-	      }, (err) => {
-	        console.log(err);
-	      });
-	  };
-
-	  TreatsController.prototype.getCandy = function() {
-	    $http.get(candyUrl)
-	      .then((res) => {
-	        this.treats = res.data.data;
-	      }, (err) => {
-	        console.log(err);
-	      });
-	  };
-
-	  // POST routes-----------------------------------
-	  TreatsController.prototype.addCookie = function() {
-	    $http.post(cookieUrl, this.treats)
-	      .then((res)=> {
-	        this.treats.push(res.data.data);
-	      }, (err) => {
-	        console.log(err);
-	      });
-	  };
-
-	  TreatsController.prototype.addCandy = function() {
-	    $http.post(candyUrl, this.treats)
-	      .then((res)=> {
-	        this.treats.push(res.data.data);
-	      }, (err) => {
-	        console.log(err);
-	      });
-	  };
-
-	  // PUT routes------------------------------------
-	  TreatsController.prototype.updateCookie = function(cookie, updateCookie) {
-	    cookie.name = updateCookie.name;
-	    cookie.edible = updateCookie.edible;
-	    cookie.stock = updateCookie.stock;
-	    $http.put(cookieUrl, cookie)
-	      .then(()=> {
-	        this.treats = this.treats.map(c => {
-	          return c._id === cookie._id ? cookie : c;
-	        });
-	      }, (err) => {
-	        console.log(err);
-	      });
-	  };
-
-	  this.updateCandy = function(candy, updateCandy) {
-	    candy.name = updateCandy.name;
-	    candy.edible = updateCandy.edible;
-	    candy.stock = updateCandy.stock;
-	    $http.put(candyUrl, candy)
-	      .then(()=> {
-	        this.treats = this.treats.map(c => {
-	          return c._id === candy._id ? candy : c;
-	        });
-	      }, (err) => {
-	        console.log(err);
-	      });
-	  };
-	  // DELETE routes---------------------------------
-
-	  this.deleteCookie = function(cookie) {
-	    $http.delete(cookieUrl + cookie._id)
-	      .then(()=> {
-	        this.treats = this.treats.filter((c)=> cookie._id !== c._id);
-	      },(err)=> {
-	        console.log(err);
-	      });
-	  };
-
-	  this.deleteCandy = function(candy) {
-	    $http.delete(candyUrl + candy._id)
-	      .then(()=> {
-	        this.treats = this.treats.filter((c)=> candy._id !== c._id);
-	      },(err)=> {
-	        console.log(err);
-	      });
-	  };
 	}
+	// GET routes------------------------------------
+
+	TreatsController.prototype.getCookies = function() {
+	  this.$http.get('http://localhost:3000/cookies')
+	    .then((res) => {
+	      this.treats = res.data;
+	    }, (err) => {
+	      console.log(err);
+	    });
+	};
+
+	TreatsController.prototype.getCandy = function() {
+	  this.$http.get(this.candyUrl)
+	    .then((res) => {
+	      this.treats = res.data;
+	    }, (err) => {
+	      console.log(err);
+	    });
+	};
+
+	// POST routes-----------------------------------
+	TreatsController.prototype.addCookie = function() {
+	  this.$http.post(this.cookieUrl, this.newCookie)
+	    .then((res)=> {
+	      this.treats.push(res.data);
+	      this.newCookie = null;
+	    }, (err) => {
+	      console.log(err);
+	    });
+	};
+
+	TreatsController.prototype.addCandy = function() {
+	  this.$http.post(this.candyUrl, this.newCandy)
+	    .then((res)=> {
+	      this.treats.push(res.data);
+	      this.newCandy = null;
+	    }, (err) => {
+	      console.log(err);
+	    });
+	};
+
+	// PUT routes------------------------------------
+	TreatsController.prototype.updateCookie = function(cookie, updateCookie) {
+	  cookie.name = updateCookie.name;
+	  cookie.edible = updateCookie.edible;
+	  cookie.stock = updateCookie.stock;
+	  this.$http.put(this.cookieUrl, cookie)
+	    .then(()=> {
+	      this.treats = this.treats.map(c => {
+	        return c._id === cookie._id ? cookie : c;
+	      });
+	    }, (err) => {
+	      console.log(err);
+	    });
+	};
+
+	TreatsController.prototype.updateCandy = function(candy, updateCandy) {
+	  candy.name = updateCandy.name;
+	  candy.edible = updateCandy.edible;
+	  candy.stock = updateCandy.stock;
+	  this.$http.put(this.candyUrl, candy)
+	    .then(()=> {
+	      this.treats = this.treats.map(c => {
+	        return c._id === candy._id ? candy : c;
+	      });
+	    }, (err) => {
+	      console.log(err);
+	    });
+	};
+	// DELETE routes---------------------------------
+
+	TreatsController.prototype.deleteCookie = function(cookie) {
+	  this.$http.delete(this.cookieUrl +'/'+ cookie._id)
+	    .then(()=> {
+	      this.treats = this.treats.filter((c)=> cookie._id !== c._id);
+	    },(err)=> {
+	      console.log(err);
+	    });
+	};
+
+	TreatsController.prototype.deleteCandy = function(candy) {
+	  this.$http.delete(this.candyUrl +'/'+ candy._id)
+	    .then(()=> {
+	      this.treats = this.treats.filter((c)=> candy._id !== c._id);
+	    },(err)=> {
+	      console.log(err);
+	    });
+	};
 
 
 /***/ }
