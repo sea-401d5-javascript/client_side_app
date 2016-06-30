223103048a0ec07c1e2c624072a5722a76681a0b
jdar22169 <mmow313@gmail.com>
2016-06-29 20:13:23 -0700

    merge

--- Maddie/app/js/twoApp/directives/FrenchieDirective.js
+++ Maddie/app/js/twoApp/directives/FrenchieDirective.js
@@ -1,10 +1,12 @@
+'use strict';
 module.exports = function(app){
   app.directive('frenchieDirective', function() {
     return {
       templateUrl:'./templates/FrenchieTemplate.html',
       scope:{
         type:'@',
-        frenchie: '=' //resource
+        frenchie: '=' //resource for config
+        //attribute for animal---dogwalker vs frenchie
       },
       require: '^^ngController',
       link: function($scope, elem, attr, controller) {
